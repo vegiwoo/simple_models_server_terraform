3c1d2190bcc1b22b2297ccf9e867f3e000745110
Dmirty S <vegiwoo@protonmail.com>
2020-01-22 15:37:49 +0300

    Add initial description in readme.md

--- backend.tf
+++ backend.tf
@@ -1,8 +1,27 @@
+// Backend specification
 terraform {
   backend "s3" {
     bucket = "sms-backend-save"
+    encrypt = true
     key    = "network/terraform.tfstate"
     region = "eu-central-1"
-    //dynamodb_table = "vgw-terra-bcknd-table"
+    dynamodb_table = "terraform-state-lock-dynamo"
+  }
+}
+
+// DynamoDB Terraform State Lock Table
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "DynamoDB Terraform State Lock Table"
   }
 }
